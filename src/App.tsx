5266581decf038f9a6d897ba9e3d64c60f30e888
Vu Hoang <vhoand860201@gmail.com>
2022-11-27 02:52:52 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import * as EthersServices from "./services/ethers.service";
+
+import "./App.css";
 
 function App() {
+  const [address, setAddress] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const onDisconnectWallet = () => {
+    setAddress("");
+    setErrorMsg("");
+  };
+
+  const onConnectWallet = async () => {
+    try {
+      const account = await EthersServices.connectWallet();
+
+      setAddress(account);
+    } catch (error: any) {
+      setErrorMsg(error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {address ? (
+        <div className="addressContainer">
+          <p className="addressText">Welcome {address}</p>
+
+          <button className="btn disconnectBtn" onClick={onDisconnectWallet}>
+            Disconnect
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button className="btn connectWalletBtn" onClick={onConnectWallet}>
+            Connect metamask
+          </button>
+        </div>
+      )}
+
+      {errorMsg && <div className="errorMsgText">{errorMsg}</div>}
     </div>
   );
 }
