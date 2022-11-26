9e7deac9aa42fab69af51884c5a4e5f2f224cb8f
Vu Hoang <vhoand860201@gmail.com>
2022-11-27 03:28:14 +0700

    detect network & account changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import React, { useState } from "react";
 
-import * as EthersServices from "./services/ethers.service";
+import { ethers } from "ethers";
 
 import "./App.css";
 
+const ethereum = (window as any)?.ethereum;
+const provider = new ethers.providers.Web3Provider(ethereum, "any");
+
 function App() {
+  const [network, setNetwork] = useState("");
   const [address, setAddress] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
 
-  const onDisconnectWallet = () => {
-    setAddress("");
-    setErrorMsg("");
+  const onChangeAccountLisener = (accounts: string[]) => {
+    setAddress(accounts[0]);
+  };
+  const onChangeNetworkListener = (newNetwork: any) => {
+    setNetwork(newNetwork.name);
   };
 
   const onConnectWallet = async () => {
     try {
-      const account = await EthersServices.connectWallet();
+      const netwotk = await provider.getNetwork();
+      setNetwork(netwotk.name);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      setAddress(accounts[0]);
 
-      setAddress(account);
+      ethereum?.on("accountsChanged", onChangeAccountLisener);
+      provider.on("network", onChangeNetworkListener);
     } catch (error: any) {
       setErrorMsg(error.message);
     }
+  };
+
+  const onDisconnectWallet = () => {
+    setAddress("");
+    setErrorMsg("");
+    ethereum?.removeListener("accountsChanged", onChangeAccountLisener);
+    provider.off("network", onChangeNetworkListener);
   };
 
   return (
     <div className="App">
       {address ? (
         <div className="addressContainer">
-          <p className="addressText">Welcome {address}</p>
+          <p className="contentText">Welcome {address}</p>
+          <p className="contentText">Network: {network}</p>
 
           <button className="btn disconnectBtn" onClick={onDisconnectWallet}>
             Disconnect
